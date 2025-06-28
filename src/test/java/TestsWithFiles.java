41b44cca995ad1b81799efd9d4ba9e85c8fb82a3
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-06-28 15:27:50 +0700

    added pdfTest

--- src/test/java/TestsWithFiles.java
+++ src/test/java/TestsWithFiles.java
@@ -1,12 +1,17 @@
+import com.codeborne.pdftest.PDF;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestsWithFiles {
     private ClassLoader cl = TestsWithFiles.class.getClassLoader();
@@ -17,9 +22,14 @@ public class TestsWithFiles {
                 cl.getResourceAsStream("file.zip")
         )) {
             ZipEntry entry;
-            while ((entry = zis.getNextEntry()) != null ) {
+            while ((entry = zis.getNextEntry()) != null) {
                 System.out.println(entry.getName());
+                if (entry.getName().equals("sample.pdf")) {
+                    PDF pdf = new PDF(zis);
+                    Assertions.assertTrue(pdf.text.contains("This PDF is three pages long"));
+                    Assertions.assertEquals("Philip Hutchison", pdf.author);
+                }
+                }
             }
         }
     }
-}
